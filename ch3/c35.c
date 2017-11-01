4592f28311ea145274bd38ef2a30b3858d894d6b
zhyucan <zhyucan@gmail.com>
2017-11-01 22:09:57 +0800

    ch3

---
+++ ch3/c35.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int sum;
+
+    // compute result
+    sum = 25 + 37 - 19;
+    // display results
+    printf("The answer is %i\n", sum);
+
+    return 0;
+}
