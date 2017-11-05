b89fd98464e19e87e59bf9eeef48ee19049e1b77
zhyucan <zhyucan@gmail.com>
2017-11-05 11:02:55 +0800

    ch11

---
+++ ch11/t1115.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int stringLength(const char *string) {
+    const char *cptr = string;
+
+    while (*cptr) {
+        cptr++;
+    }
+
+    return cptr - string;
+}
+
+int main(void) {
+    int stringLength(const char *string);
+
+    printf("%i ", stringLength("stringLength test"));
+    printf("%i ", stringLength(" "));
+    printf("%i\n", stringLength("complete"));
+
+    return 0;
+}
